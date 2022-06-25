d77c1348efd2f9834e2537e026aafdcbddf27d48
Mohamed Tharwat <88713010+Mo-Tharwat@users.noreply.github.com>
2022-06-25 16:20:08 +0200

    Complete the rest of APIs

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,20 +31,7 @@ $('.strip-toggel-menu').click(function(){
 
 
 // --------------------------------------------- API ---------------------------------------------
-//Select variables UI
-let navItems        = $('.navItem');
-let searchInputs    = $('.inputsSearch');
-let searchURL       = "https://api.themoviedb.org/3/search/movie?query=mad&api_key=90cb39278979ed66254217668439759f&language=en-US&include_adult=false";
-let searchArray     = [];
-
-//Main arrays from API sideNav
-let nowPlayingArray = [];
-let popularArray    = [];
-let topRatedArray   = [];
-let trendingArray   = [];
-let upComingArray   = [];
-let latestArray     = [];
-
+//API obj
 let api = {
     url:{
         nowPlaying  :"https://api.themoviedb.org/3/movie/now_playing?api_key=90cb39278979ed66254217668439759f",
@@ -53,41 +40,126 @@ let api = {
         trending    :"https://api.themoviedb.org/3/trending/all/day?api_key=90cb39278979ed66254217668439759f",
         upComing    :"https://api.themoviedb.org/3/movie/upcoming?api_key=90cb39278979ed66254217668439759f",
         latestURL   :"https://api.themoviedb.org/3/movie/latest?api_key=90cb39278979ed66254217668439759f",
-    },
-
-};
-
-
-// window.addEventListener('ready',function(){
-
-
-
-// });
-
-
-
-async function loadAPInavItem(argument = api.url.nowPlaying){
-
-    let nowPlayingAPI   = await fetch(argument);
-    
-    if(nowPlayingAPI.ok && nowPlayingAPI.status == 200){
-        console.log('Now Playing API is ready');
-        let nowPlayingJson  = await nowPlayingAPI.json();
-        nowPlayingArray     = nowPlayingJson.results;
-        console.log(nowPlayingArray);
-    }else{
-        console.log('Now Playing API is not ready');
     }
-
 };
 
-loadAPInavItem();
+let arraysearch = [];
 
+$(document).ready(function(){
 
-
-$('.navItem').click(function(){
-        console.log($(this).attr('id'));
+    loadAPInavItem(api.url.nowPlaying);
+    // Click on navItem
+    $('.navItem').click(function(){
+        // console.log($(this).attr('id'));
+        if($(this).attr('id') == 'nowPlaying'){
+            console.log($(this).val());
+            loadAPInavItem(api.url.nowPlaying);
+        }else if($(this).attr('id') == 'popular'){
+            console.log(this.value == 'popular');
+            loadAPInavItem(api.url.popular);
+        }else if($(this).attr('id') == 'topRated'){
+            console.log($(this).val());
+            loadAPInavItem(api.url.topRated);
+        }else if($(this).attr('id') == 'trending'){
+            console.log($(this).val());
+            loadAPInavItem(api.url.trending);
+        }else if($(this).attr('id') == 'upComing'){
+            console.log($(this).val());
+            loadAPInavItem(api.url.upComing);
+        }
+        // else if($(this).attr('id') == 'contact'){
+        //     let contact = $('#contactUs').offset().top;
+        //     console.log(contact);
+        //     $('body').animate({scrollTop:contact},10000);
+        // }
+    });
+    // Search on API
+    $('#searchOnAPI').keyup(function(){
+        let search = `https://api.themoviedb.org/3/search/movie?query=${this.value}&api_key=90cb39278979ed66254217668439759f&language=en-US&include_adult=false`;
+        loadAPInavItem(search);
+    });
+    // Search on movies
+    $('#searchArray').keyup(function(){
+        console.log(arraysearch);
+        let arrayList = [];
+        for(let i=0; i<arraysearch.length; i++){
+            if(String(arraysearch[i].original_title).toLowerCase().includes(this.value)){
+                arrayList.push(arraysearch[i]);
+            }
+        }
+        //console.log(arrayList);
+        filterMovies(arrayList);
+        //$('#searchMovies').html(htmlTags);
+    });
 });
+
+
+async function loadAPInavItem(url){
+    let fetchAPI    = await fetch(url);
+    let jsonAPI     = await fetchAPI.json();
+    let arrayJson = Array.from(jsonAPI.results);
+    console.log(arrayJson);
+    // let arrayJson = jsonAPI.results;
+    // console.log(arrayJson);
+    displayMovies(arrayJson);
+    arraysearch = arrayJson;
+};
+
+function displayMovies(parameter){
+    let htmlTags = '';
+    //console.log("from Display: "+ parameter);
+    for (let i = 0; i < parameter.length; i++) {
+        htmlTags += `
+        <div class="col-lg-4 col-md-6">
+        <figure class="position-relative rounded-1 overflow-hidden">
+            <img class="w-100 rounded-1" src="https://image.tmdb.org/t/p/w500${parameter[i].poster_path}" alt="${parameter[i].title}">
+            <div class="layer position-absolute rounded-1 w-100 h-100">
+                <div class="info p-4 text-center m-0">
+
+                    <h2>${parameter[i].original_title}</h2>
+                    <p>${parameter[i].overview}</p>
+                    <p>rate: ${parameter[i].vote_average}</p>
+                    <p>${parameter[i].release_date}</p>
+                    
+                </div>
+            </div>
+        </figure>
+
+    </div>
+        `;
+    }
+    // console.log(htmlTags);
+    $('#showMovies').html(htmlTags);
+};
+
+function filterMovies(parameter){
+    let htmlTags = '';
+    //console.log("from Display: "+ parameter);
+    for (let i = 0; i < parameter.length; i++) {
+        htmlTags += `
+        <div class="col-lg-4 col-md-6">
+        <figure class="position-relative rounded-1 overflow-hidden">
+            <img class="w-100 rounded-1" src="https://image.tmdb.org/t/p/w500${parameter[i].poster_path}" alt="${parameter[i].title}">
+            <div class="layer position-absolute rounded-1 w-100 h-100">
+                <div class="info p-4 text-center m-0">
+
+                    <h2>${parameter[i].original_title}</h2>
+                    <p>${parameter[i].overview}</p>
+                    <p>rate: ${parameter[i].vote_average}</p>
+                    <p>${parameter[i].release_date}</p>
+                    
+                </div>
+            </div>
+        </figure>
+
+    </div>
+        `;
+    }
+    // console.log(htmlTags);
+    $('#searchMovies').html(htmlTags);
+};
+
+
 
 
 // --------------------------------------------- contact us ---------------------------------------------
